619670cb5e966227e5f5575c003042cae9903821
Tushar Shukla <shuklatuhsar100@gmail.com>
2025-08-14 20:19:31 +0530

    working changes

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
+  project: {
+    android: {
+      sourceDir: 'android',
+      appName: 'app',
+      packageName: 'com.hyperswitchsdkreactnativeexample',
+    },
+  },
   dependencies: {
     [pak.name]: {
       root: path.join(__dirname, '..'),
